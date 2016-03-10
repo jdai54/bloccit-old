68ab644104dd65ad0a7d436413ccb807c4690fa2
Jake <jdai54@gmail.com>
2016-03-10 10:56:38 -0800

    Assignment 42 incomplete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
-  has_many :commentings, as: :commentable
-  has_many :comments, through: :commentings, dependent: :destroy
+  has_many :comments, as: :commentable 
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   default_scope { order('created_at DESC') }
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,5 @@
 class Topic < ActiveRecord::Base
-  has_many :commentings, as: :commentable
-  has_many :comments, through: :commentings, dependent: :destroy
+  has_many :comments, as: :commentable 
   has_many :posts, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
