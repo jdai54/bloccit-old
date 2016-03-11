820828564794eb70d6570e6c1b06b41ca8ffa36b
Jake <jdai54@gmail.com>
2016-03-11 02:22:16 -0800

    Checkpoint 43 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309040212) do
+ActiveRecord::Schema.define(version: 20160310200126) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -35,12 +35,15 @@ ActiveRecord::Schema.define(version: 20160309040212) do
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
   end
 
+  add_index "comments", ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
   add_index "comments", ["topic_id"], name: "index_comments_on_topic_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
@@ -69,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160309040212) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
